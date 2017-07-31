f3a9f0a9cc1eec6cf3507ba8f18de18f3d6e529d
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2017-07-31 16:27:08 -0400

    Add Rat base

---
+++ Rongeurville/Rongeurville/Rat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPI;
+
+namespace Rongeurville
+{
+    public class Rat:Actor
+    {
+        private bool ShouldDie { get; set; } = false;
+        private Intracommunicator comm;
+
+
+        public Rat()
+        {
+            comm = Communicator.world;
+            DoRatThings();
+        }
+
+        private void DoRatThings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Tile> GetNeighboors(Tile center)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
