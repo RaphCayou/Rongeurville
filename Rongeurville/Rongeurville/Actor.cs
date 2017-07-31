80530217b9f6155592080ed0a2609bf9ffbabe5c
jeepee12 <jeepee.12@hotmail.com>
2017-07-31 13:10:03 -0400

    Adding Tile and Actor classes.

---
+++ Rongeurville/Rongeurville/Actor.cs
@@ -0,0 +1,10 @@
+﻿namespace Rongeurville
+{
+    public class Actor
+    {
+        public Tile GetDirectionWithAStar(Tile start, Tile target)
+        {
+            return new Tile();
+        }
+    }
+}
---
+++ Rongeurville/Rongeurville/Tile.cs
@@ -0,0 +1,16 @@
+﻿namespace Rongeurville
+{
+    public class Tile
+    {
+        public enum TileContent
+        {
+            Empty,
+            Wall,
+            Cheese,
+            Cat,
+            Rat,
+        }
+        public int X;
+        public int Y;
+    }
+}
