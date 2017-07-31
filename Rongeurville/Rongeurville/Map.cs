497e64e71936a0381911d57bc1fe03fa7a752b3c
fvigneault <minivigno@gmail.com>
2017-07-31 14:27:00 -0400

    Creation of map class

---
+++ Rongeurville/Rongeurville/Map.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rongeurville
+{
+    class Map
+    {
+        public Map()
+        {
+            Console.WriteLine("Map constructed.");
+        }
+    }
+}
