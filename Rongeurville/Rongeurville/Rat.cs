ec8a6a55659dc55e3d4da70d12258674a7452d0b
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2017-07-31 16:51:00 -0400

    Continue Rat

--- Rongeurville/Rongeurville/Rat.cs
+++ Rongeurville/Rongeurville/Rat.cs
@@ -9,9 +9,11 @@ namespace Rongeurville
 {
     public class Rat:Actor
     {
-        private bool ShouldDie { get; set; } = false;
+        private bool shouldDie = false;
+        private bool shouldBeScared = false;
+        private int timeSinceLastMeow = 0;
+        private Tile lastMeow;
         private Intracommunicator comm;
-
 
         public Rat()
         {
@@ -21,11 +23,50 @@ namespace Rongeurville
 
         private void DoRatThings()
         {
+            // Listen for Meows
+
+
+            List<Tile> objectiveList = shouldBeScared ? map.Exits : map.Cheese;
+            int closestObjectiveDistance = int.MaxValue;
+            Tile closestObjective = null;
+            foreach (var objective in objectiveList)
+            {
+                Tuple<Tile, int> aStarResult = GetDirectionWithAStar(objective);
+                if (aStarResult.Item2 == -1) continue;
+                if (aStarResult.Item2 >= closestObjectiveDistance) continue;
+                closestObjectiveDistance = aStarResult.Item2;
+                closestObjective = objective;
+            }
+
+
+            // MOVE
+
+
+            if (timeSinceLastMeow > 0)
+            {
+                timeSinceLastMeow -= 1;
+            }
             throw new NotImplementedException();
         }
 
         public override List<Tile> GetNeighboors(Tile center)
         {
+            // UP
+
+            // UP RIGHT
+
+            // RIGHT
+
+            // DOWN RIGHT
+
+            // DOWN
+
+            // DOWN LEFT
+
+            // LEFT
+
+            // UP LEFT
+
             throw new NotImplementedException();
         }
     }
