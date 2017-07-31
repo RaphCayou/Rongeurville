d5e72cbe9d1f7dbeb07c41747818a87d56ffc004
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2017-07-31 14:31:16 -0400

    Add cat base

---
+++ Rongeurville/Rongeurville/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MPI;
+
+namespace Rongeurville
+{
+    public class Cat
+    {
+        private bool ShouldDie { get; set; } = false;
+
+        public Cat()
+        {
+            //Task.Run(() => DoCatThings());
+            DoCatThings();
+        }
+
+        private void DoCatThings()
+        {
+            while (!ShouldDie)
+            {
+                //Get closest rat
+                //if rat <= 10 tiles, MEOW
+                //Get path to the rat
+                //Send move command to map
+                //Listen for new position or die
+            }
+        }
+
+        //MEOW
+
+    }
+}
