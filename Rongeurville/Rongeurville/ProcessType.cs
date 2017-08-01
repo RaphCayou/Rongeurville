5ab0822aea1b1d8e0f3c2d4a0dbe824eff391ac1
fvigneault <minivigno@gmail.com>
2017-08-01 17:05:23 -0400

    Added ProcessType

---
+++ Rongeurville/Rongeurville/ProcessType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rongeurville
+{
+    public enum ProcessType
+    {
+        Map,
+        Rat,
+        Cat,
+    }
+}
