756dead1f05f97fa8bb92d66c2faf89c86370023
Jonathan Martineau <jo_martineau_7@hotmail.com>
2017-08-04 10:37:58 -0400

    Fixed exits with Unit Tests!

---
+++ Rongeurville/RongeurvilleTests/MapTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rongeurville;
+
+namespace RongeurvilleTests
+{
+    [TestClass]
+    public class MapTests
+    {
+        private const string TEST_MAP = "###########################################\n" +
+                                        "#   F    F               C            #   #\n" +
+                                        "#                                     # R #\n" +
+                                        "#     ##################       ########   #\n" +
+                                        "#     #          R     #          F   #   #\n" +
+                                        "#     #  F   #         #     ###      #   #\n" +
+                                        "      #      #####     #     #    #       #\n" +
+                                        "#     #######        ###   ###    #       #\n" +
+                                        "# R                               #   ##  #\n" +
+                                        "#      ####         C         #   #\n" +
+                                        "###########################################";
+        [TestMethod]
+        public void TestExits()
+        {
+            Map map = Map.ParseMap(TEST_MAP);
+
+            Assert.AreEqual(2, map.Exits.Count);
+            Assert.AreEqual(new Coordinates {X=0, Y=6}, map.Exits[0].Position);
+            Assert.AreEqual(new Coordinates {X=42, Y=9}, map.Exits[1].Position);
+        }
+    }
+}
